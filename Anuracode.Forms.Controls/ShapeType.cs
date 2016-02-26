57a00e981975b7da947572d237630b864bffa369
Alberto Puyana <apuyana@anuracode.com>
2016-02-26 11:34:27 -0500

    Sample base projects added.

---
+++ Anuracode.Forms.Controls/ShapeType.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="ShapeType.cs" company="">
+// All rights reserved.
+// </copyright>
+
+namespace Anuracode.Forms.Controls
+{
+    /// <summary>
+    /// Shape type.
+    /// </summary>
+    public enum ShapeType
+    {
+        /// <summary>
+        /// Box view.
+        /// </summary>
+        Box,
+
+        /// <summary>
+        /// Circle view.
+        /// </summary>
+        Circle
+    }
+}
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Set of control used in Xamarin.Forms.
 Most of the control were inspired by open source development, improved (rewritten in some cases) and cleaned.
 
 This is the list of sources used for inspiration:
+
 Xamarin Labls https://github.com/XLabs/Xamarin-Forms-Labs
 For the extended label and entry.
 
