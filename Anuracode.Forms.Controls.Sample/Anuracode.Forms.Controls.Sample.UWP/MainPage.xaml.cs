b49fabd32a1cc431cfdf63effa9d17659e182d3f
Alberto Puyana <apuyana@anuracode.com>
2016-03-14 16:49:51 -0500

    Nuget info completed.

--- Anuracode.Forms.Controls.Sample/Anuracode.Forms.Controls.Sample.UWP/MainPage.xaml.cs
+++ Anuracode.Forms.Controls.Sample/Anuracode.Forms.Controls.Sample.UWP/MainPage.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using System.Reflection;
 
 namespace Anuracode.Forms.Controls.Sample.UWP
 {
@@ -21,7 +10,48 @@ namespace Anuracode.Forms.Controls.Sample.UWP
         {
             this.InitializeComponent();
 
+            RegisterRenderers();
+
             LoadApplication(new Anuracode.Forms.Controls.Sample.App());
+        }
+
+        /// <summary>
+        /// Register renderers.
+        /// </summary>
+        private void RegisterRenderers()
+        {
+            string registrarType = "Xamarin.Forms.Registrar, Xamarin.Forms.Core";
+            object registrarInstance;
+            MethodInfo registerMethod;
+
+            var type = Type.GetType(registrarType, true);
+
+            string propertyName = "Registered";
+
+            var property = (from propertySelect in type.GetRuntimeProperties()
+                            where propertySelect.Name == propertyName
+                            select propertySelect).FirstOrDefault();
+
+            if (property != null)
+            {
+                registrarInstance = property.GetValue(null);
+
+                string methodName = "Register";
+
+                registerMethod = (from methodSelect in property.PropertyType.GetRuntimeMethods()
+                                  where methodSelect.Name == methodName
+                                  select methodSelect).FirstOrDefault();
+
+                if (registerMethod != null)
+                {
+                    // This renderers are not using the theme colors.
+                    registerMethod.Invoke(registrarInstance, new object[] { typeof(ExtendedLabel), typeof(Renderers.ExtendedLabelRenderer) });
+                    registerMethod.Invoke(registrarInstance, new object[] { typeof(ExtendedEntry), typeof(Renderers.ExtendedEntryRenderer) });
+                    registerMethod.Invoke(registrarInstance, new object[] { typeof(ExtendedImage), typeof(Renderers.ManagedImageRenderer) });
+                    registerMethod.Invoke(registrarInstance, new object[] { typeof(ShapeView), typeof(Renderers.ShapeRenderer) });
+                    registerMethod.Invoke(registrarInstance, new object[] { typeof(SignaturePadView), typeof(Renderers.SignaturePadViewRenderer) });
+                }
+            }
         }
     }
 }
