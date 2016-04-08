198f01987441621fb2c0a207ac364cbb58ddb20c
Alberto Puyana <apuyana@anuracode.com>
2016-04-08 14:35:51 -0500

    Managed SimpleView.

--- Anuracode.Forms.Controls/Layouts/SimpleViewBase.cs
+++ Anuracode.Forms.Controls/Layouts/SimpleViewBase.cs
@@ -3,8 +3,8 @@
 // </copyright>
 // <author>Alberto Puyana</author>
 
-using Xamarin.Forms;
 using Anuracode.Forms.Controls.Extensions;
+using Xamarin.Forms;
 
 namespace Anuracode.Forms.Controls
 {
@@ -32,8 +32,8 @@ namespace Anuracode.Forms.Controls
                 Padding = 0
             };
 
-            ContentLayout.OnLayoutChildren += ContentLayout_OnLayoutChildren;
-            ContentLayout.ManualSizeCalculationDelegate = ContentLayout_OnSizeRequest;
+            ContentLayout.OnLayoutChildren += InternalContentLayout_OnLayoutChildren;
+            ContentLayout.ManualSizeCalculationDelegate = InternalContentLayout_OnSizeRequest;
 
             if (autoInit)
             {
@@ -113,11 +113,6 @@ namespace Anuracode.Forms.Controls
         public virtual void RecycleView()
         {
         }
-
-        /// <summary>
-        /// Setup the bindings of the elements.
-        /// </summary>
-        protected abstract void SetupBindings();
 
         /// <summary>
         /// Set up cell values.
@@ -203,6 +198,51 @@ namespace Anuracode.Forms.Controls
 
                 return new SizeRequest(new Size(requestWidth, requestHeight), new Size(minimumWidth, minimumHeight));
             }
+        }
+
+        /// <summary>
+        /// Setup the bindings of the elements.
+        /// </summary>
+        protected abstract void SetupBindings();
+
+        /// <summary>
+        /// Layout children.
+        /// </summary>
+        /// <param name="x">Top to use.</param>
+        /// <param name="y">Left to use.</param>
+        /// <param name="width">Width to use.</param>
+        /// <param name="height">Height to use.</param>
+        private void InternalContentLayout_OnLayoutChildren(double x, double y, double width, double height)
+        {
+            try
+            {
+                ContentLayout_OnLayoutChildren(x, y, width, height);
+            }
+            catch (System.Exception ex)
+            {
+                AC.TraceError("OnSize error", ex);
+            }
+        }
+
+        /// <summary>
+        /// Mesure the content.
+        /// </summary>
+        /// <param name="widthConstraint">Width to constarint.</param>
+        /// <param name="heightConstraint">Height to constraint.</param>
+        /// <returns>Size to use.</returns>
+        private SizeRequest InternalContentLayout_OnSizeRequest(double widthConstraint, double heightConstraint)
+        {
+            SizeRequest returnValue = new SizeRequest();
+            try
+            {
+                returnValue = ContentLayout_OnSizeRequest(widthConstraint, heightConstraint);
+            }
+            catch (System.Exception ex)
+            {
+                AC.TraceError("OnSize error", ex);
+            }
+
+            return returnValue;
         }
     }
 }
