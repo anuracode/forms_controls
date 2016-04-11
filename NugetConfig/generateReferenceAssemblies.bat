2f01003db8670a22d14738cea2ac36d0a1ffdf4f
Alberto Puyana <apuyana@anuracode.com>
2016-04-11 15:33:29 -0500

    Nuget definition updated.

---
+++ NugetConfig/generateReferenceAssemblies.bat
@@ -0,0 +1,3 @@
+mkdir "C:\Proyectos\forms_controls\Anuracode.Forms.Controls.WindowsUWP\bin\X86\Release\Ref"
+xcopy /Y "C:\Proyectos\forms_controls\Anuracode.Forms.Controls.WindowsUWP\bin\X86\Release\Anuracode.Forms.Controls.WindowsUWP.dll" "C:\Proyectos\forms_controls\Anuracode.Forms.Controls.WindowsUWP\bin\X86\Release\Ref\Anuracode.Forms.Controls.WindowsUWP.dll"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\CorFlags.exe"  /32bitreq- C:\Proyectos\forms_controls\Anuracode.Forms.Controls.WindowsUWP\bin\X86\Release\Ref\Anuracode.Forms.Controls.WindowsUWP.dll
