46b137fd27762c2f6a68525d4ba40c3136545a80
Alberto Puyana <apuyana@anuracode.com>
2017-01-30 11:13:48 -0500

    iOS renderer fixed updated.

--- Anuracode.Forms.Controls.Sample/Anuracode.Forms.Controls.Sample.iOS/AppDelegate.cs
+++ Anuracode.Forms.Controls.Sample/Anuracode.Forms.Controls.Sample.iOS/AppDelegate.cs
@@ -1,4 +1,7 @@
-﻿using Foundation;
+﻿using FFImageLoading;
+using FFImageLoading.Helpers;
+using Foundation;
+using System;
 using UIKit;
 
 namespace Anuracode.Forms.Controls.Sample.iOS
@@ -19,11 +22,42 @@ namespace Anuracode.Forms.Controls.Sample.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             Anuracode.Forms.Controls.Renderers.ExtendedImageRenderer.AllowDownSample = true;
+            Anuracode.Forms.Controls.Renderers.ExtendedImageRenderer.Init();
+
             global::Xamarin.Forms.Forms.Init();
+
+            var config = new FFImageLoading.Config.Configuration()
+            {
+                VerboseLogging = false,
+                VerbosePerformanceLogging = false,
+                VerboseMemoryCacheLogging = false,
+                VerboseLoadingCancelledLogging = false,
+                Logger = new CustomLogger(),
+            };
+
+            ImageService.Instance.Initialize(config);
 
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
+        }
+    }
+
+    public class CustomLogger : IMiniLogger
+    {
+        public void Debug(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Error(string errorMessage)
+        {
+            Console.WriteLine(errorMessage);
+        }
+
+        public void Error(string errorMessage, Exception ex)
+        {
+            Error(errorMessage + System.Environment.NewLine + ex.ToString());
         }
     }
 }
