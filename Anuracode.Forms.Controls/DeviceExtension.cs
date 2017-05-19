fcdc0a389f63d0c880d266e83fbde3bed7e7c650
Alberto Puyana <apuyana@anuracode.com>
2017-05-18 19:58:27 -0500

    Nuget updated, revert to android 6.

--- Anuracode.Forms.Controls/DeviceExtension.cs
+++ Anuracode.Forms.Controls/DeviceExtension.cs
@@ -39,5 +39,47 @@ namespace Anuracode.Forms.Controls
         /// Represent Windows 8 platfrom.
         /// </summary>
         public const string WinRT = "WinRT";
+
+        /// <summary>
+        /// Platform delegate.
+        /// </summary>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="os">Os to use.</param>
+        /// <param name="iOS">Delegate to use.</param>
+        /// <param name="android">Delegate to use.</param>
+        /// <param name="windowsPhone">Delegate to use.</param>
+        /// <param name="windows">Delegate to use.</param>
+        /// <param name="other">Delegate to use.</param>
+        /// <returns>Value to use.</returns>
+        public static TValue OnPlatform<TValue>(this string os, TValue iOS, TValue android, TValue windowsPhone, TValue windows, TValue other = default(TValue))
+        {
+            TValue returnValue = default(TValue);
+
+            switch (os)
+            {
+                case DeviceExtension.Android:
+                    returnValue = android;
+                    break;
+
+                case DeviceExtension.WinPhone:
+                    returnValue = windowsPhone;
+                    break;
+
+                case DeviceExtension.UWP:
+                case DeviceExtension.WinRT:
+                    returnValue = windows;
+                    break;
+
+                case DeviceExtension.iOS:
+                    returnValue = iOS;
+                    break;
+
+                default:
+                    returnValue = other;
+                    break;
+            }
+
+            return returnValue;
+        }
     }
 }
--- Anuracode.Forms.Controls/ObjectExtension.cs
+++ Anuracode.Forms.Controls/ObjectExtension.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
 namespace Anuracode.Forms.Controls.Extensions
 {
@@ -60,48 +59,6 @@ namespace Anuracode.Forms.Controls.Extensions
                 });
 
             return tc.Task;
-        }        
-
-        /// <summary>
-        /// Platform delegate.
-        /// </summary>
-        /// <typeparam name="TValue">Type of the value.</typeparam>
-        /// <param name="os">Os to use.</param>
-        /// <param name="iOS">Delegate to use.</param>
-        /// <param name="android">Delegate to use.</param>
-        /// <param name="windowsPhone">Delegate to use.</param>
-        /// <param name="windows">Delegate to use.</param>
-        /// <param name="other">Delegate to use.</param>
-        /// <returns>Value to use.</returns>
-        public static TValue OnPlatform<TValue>(this string os, TValue iOS, TValue android, TValue windowsPhone, TValue windows, TValue other = default(TValue))
-        {
-            TValue returnValue = default(TValue);
-
-            switch (os)
-            {
-                case DeviceExtension.Android:
-                    returnValue = android;
-                    break;
-
-                case DeviceExtension.WinPhone:
-                    returnValue = windowsPhone;
-                    break;
-
-                case DeviceExtension.UWP:
-                case DeviceExtension.WinRT:
-                    returnValue = windows;
-                    break;
-
-                case DeviceExtension.iOS:
-                    returnValue = iOS;
-                    break;
-
-                default:
-                    returnValue = other;
-                    break;
-            }
-
-            return returnValue;
         }
 
         /// <summary>
